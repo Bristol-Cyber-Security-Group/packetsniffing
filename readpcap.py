34bdf4e195b2e14bbe8c7bb5f895e7eb1d7e2847
parthadc9 <partha.daschowdhury@bristol.ac.uk>
2022-03-01 15:03:05 +0000

    With location

--- readpcap.py
+++ readpcap.py
@@ -4,35 +4,34 @@ import socket
 from struct import pack
 import pyshark
 import csv
+import json
 from scapy.all import *
+import urllib.request as ur
 
 
 def readcap(packet):
-    # print(packet)
-    # return
-    with open("csvfrommessages.csv", "a", encoding="UTF8") as f: # change file name
-
+    load_layer('tls')
+    with open("csvfrommessagesv6.csv", "a", encoding="UTF8") as f:  # change file name
         writer = csv.writer(f)
-
         try:
+             if ARP in packet and packet[ARP].op in (1, 2):
+               row = [packet[ARP].hwsrc, packet[ARP].psrc, 0, 0, 0, 0]
+               writer.writerow(row)
 
-            if ARP in packet and packet[ARP].op in (1, 2):  # who-has or is-at
-
-                row = [packet[ARP].hwsrc, packet[ARP].psrc, 0, 0 ,0]
-
-                writer.writerow(row)
-
-            else:
-                dstName = socket.gethostbyaddr(packet[IP].dst)
-                rows = [packet[IP].src, packet[IP].dst,
-                    dstName, packet[IP].proto, packet[IP].len]
-                writer.writerow(rows)
+             else:
+                    dstName = socket.gethostbyaddr(packet[IP].dst)
+                    with ur.urlopen("https://geolocation-db.com/jsonp/"+packet[IP].dst) as url:
+                        data = url.read().decode()
+                        data = data.split("(")[1].strip(")")
+                        rows = [packet[IP].src, packet[IP].dst,
+                                dstName, packet[IP].proto, packet[IP].len, data]
+                        writer.writerow(rows)
 
         except Exception as e:
-            #print(e)
-            # print("==================", ARP in packet)
-            pass
+           #print(e)
+          pass
 
 
+cap = sniff(offline='messages.pcapng', prn=readcap)  # change file name
 
-cap = sniff(offline = 'messages.pcapng', prn=readcap) # change file name
+# cap = sniff(offline='messages.pcapng', prn=readcap)
