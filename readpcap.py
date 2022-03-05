0ba6fd55135b69cac28b4fb6e42af048cd041316
parthadc9 <partha.daschowdhury@bristol.ac.uk>
2022-03-05 14:40:51 +0000

    Country Name

--- readpcap.py
+++ readpcap.py
@@ -11,7 +11,7 @@ import urllib.request as ur
 
 def readcap(packet):
     load_layer('tls')
-    with open("csvfrommessageswithoutlocation.csv", "a", encoding="UTF8") as f:  # change file name
+    with open("csvfrommessages.csv", "a", encoding="UTF8") as f:  # change file name
         writer = csv.writer(f)
         try:
              if ARP in packet and packet[ARP].op in (1, 2):
@@ -22,9 +22,9 @@ def readcap(packet):
                     dstName = socket.gethostbyaddr(packet[IP].dst)
                     with ur.urlopen("https://geolocation-db.com/jsonp/"+packet[IP].dst) as url:
                         data = url.read().decode()
-                        data = data.split("(")[1].strip(")")
+                        data = json.loads(data.split("(")[1].strip(")"))
                         rows = [packet[IP].src, packet[IP].sport, packet[IP].dst,packet[IP].dport, 
-                                dstName, packet[IP].proto, packet[IP].len, packet[IP].tos] 
+                                dstName, packet[IP].proto, packet[IP].len, packet[IP].tos, data['country_name']] 
                         writer.writerow(rows)
 
         except Exception as e:
