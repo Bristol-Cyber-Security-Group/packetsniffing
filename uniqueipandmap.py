23534e6f6480f33e5c859d8d2ea9a4c2ee005348
parthadc9 <partha.daschowdhury@bristol.ac.uk>
2022-03-06 14:54:07 +0000

    Unique IPs and Map

---
+++ uniqueipandmap.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import urllib.request as ur
+import json
+import csv
+import socket
+import gmplot
+import numpy as np
+
+file = pd.read_csv("name.csv") #Please insert the csv file
+colors = ['red', 'blue', 'green', 'purple',
+          'orange', 'yellow', 'pink', 'white']
+Latitude = []
+Longitude = []
+
+try:
+    file2 = pd.DataFrame(file.groupby(['Destination']).count())
+    for i in file2.index:
+        try:
+            with ur.urlopen("https://geolocation-db.com/jsonp/"+i) as url:
+                data = url.read().decode()
+                data = json.loads(data.split("(")[1].strip(")"))
+                dstname = socket.gethostbyaddr(i)
+                print(i, dstname, data['country_name'])
+                Latitude.append(float(data['latitude']))
+                Longitude.append(float(data['longitude']))
+        except Exception as e:
+            print(e)
+            pass
+    gmap = gmplot.GoogleMapPlotter(Latitude[0],Longitude[0], 5)
+    gmap.scatter(Latitude,Longitude, colors[0], edge_width=10)
+    gmap.polygon(Latitude, Longitude, color = 'cornflowerblue')
+    gmap.apikey = "AIzaSyA9I9P6BiSTthk7K5VkZ0Ms79DLPcVRo4E"
+    gmap.draw("map.html")
+except Exception as e:
+  print(e)  
+  pass
+
