147068b1b7c0ed19bc6771c6c38d2b8b2795a1e6
Maria Sameen <42914723+MariaSameen@users.noreply.github.com>
2022-04-19 03:59:59 +0100

    Update ALM_Apriori.py

--- ALM_Apriori.py
+++ ALM_Apriori.py
@@ -1,5 +1,7 @@
-!nvidia-smi
+# To see which type of resoucres like GPU have been alloted - for Google Colab
+!nvidia-smi 
 
+# To install and import main libraries
 !pip install apriori
 
 !pip install apyori
@@ -9,19 +11,26 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from apyori import apriori
 
+# To download the dataset from repo
 ! git clone https://github.com/Bristol-Cyber-Security-Group/packetsniffing.git
 
+# To import and view the dataset in dataframes
 store_data = pd.read_csv('csvfrommessageswithoutlocation.csv')
 
 store_data
 
+# To convert dataframe into a list of lists
 records = []
 for i in range(len(store_data)):
     records.append([str(store_data.values[i,j]) for j in range(0, 6)])
 
+# To apply Apriori Algorithm 
 association_rules = apriori(records, min_support=0.0045, min_confidence=0.2, min_lift=3, min_length=2)
+
+# To convert and view the list of rules
 association_results = list(association_rules)
 
+# To view the rules with support, confidence and lift
 for item in association_results:
   print("Rule: ", list(item[0])) 
   print("Support: " + str(item[1]))
